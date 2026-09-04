b8beaf8464d9a6c36dde526bb1f26b8066a4b9da
Ishwa-K <ishwa.kkip@gmail.com>
2026-09-04 23:49:07 +0530

    Delete LABs directory

--- LABs/Unit1/ex1.py
+++
@@ -1,8 +0,0 @@
-#Write and execute a basic Python program. This demonstrates variables, input(), output(), and a simple calculation.
-name = input("Enter your name: ") 
-age = int(input("Enter your age: ")) 
-print("\n--- Student Details ---") 
-print("Name:", name) 
-print("Age:", age) 
-print("Age next year:", age + 1) 
-print(f"Hello {name}! Welcome to Python Programming.")
